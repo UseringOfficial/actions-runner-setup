7a1d20056a0000cb667086076c19dc298bb16208
delai <shidelai@gmail.com>
2024-01-29 10:56:23 +0800

    set git alias

--- 7. final.sh
+++ 7. final.sh
@@ -1,2 +1,9 @@
+echo 'set git alias'
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.pl pull
+git config --global alias.ps push
 
 echo 'Please add '//npm.pkg.github.com/:_authToken=GITHUB_NPM_TOKEN' into ~/.npmrc, if you use github private npm registry'
