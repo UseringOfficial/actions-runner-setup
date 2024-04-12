d921f6a8932ab85c70ad07b8777d30647e10e44a
delai <shidelai@gmail.com>
2024-04-13 00:26:55 +0800

    fix: The quote after "noproxy" has been deleted.

--- 6.set_proxy.sh
+++ 6.set_proxy.sh
@@ -1,7 +1,7 @@
 # only for ssh login user
-echo '
+echo "
 export all_proxy="http://127.0.0.1:7890"
 export http_proxy="http://127.0.0.1:7890"
 export https_proxy="http://127.0.0.1:7890"
 alias noproxy='unset all_proxy https_proxy http_proxy'
-' >> ~/.bashrc
+" >> ~/.bashrc
