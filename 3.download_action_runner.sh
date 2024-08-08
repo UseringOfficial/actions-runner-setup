fd2f4d4aacbccb09919f5644decda1e6dfa03f89
delai <shidelai@gmail.com>
2024-08-08 22:05:27 +0800

    upgrade runner package

--- 3.download_action_runner.sh
+++ 3.download_action_runner.sh
@@ -1,4 +1,4 @@
-runner_is_download=`echo "29fc8cf2dab4c195bb147384e7e2c94cfd4d4022c793b346a6175435265aa278  ./runners/actions-runner-linux-x64-2.311.0.tar.gz" | shasum -a 256 -c`
+runner_is_download=`echo "9e883d210df8c6028aff475475a457d380353f9d01877d51cc01a17b2a91161d  ./runners/actions-runner-linux-x64-2.317.0.tar.gz" | shasum -a 256 -c`
 
 echo $runner_is_download
 
@@ -6,5 +6,5 @@ if [[ $runner_is_download == *"OK" ]]; then
     echo "runner is already download"
 else
     echo "runner is not download, start download"
-    curl -o runners/actions-runner-linux-x64.tar.gz -L "https://github.com/actions/runner/releases/download/v2.311.0/actions-runner-linux-x64-2.311.0.tar.gz"
+    curl -o runners/actions-runner-linux-x64.tar.gz -L "https://github.com/actions/runner/releases/download/v2.317.0/actions-runner-linux-x64-2.317.0.tar.gz"
 fi
