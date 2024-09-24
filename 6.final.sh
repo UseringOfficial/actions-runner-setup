785842e1451fb5e905e96443d7e299c279dd1736
delai <shidelai@gmail.com>
2024-09-24 22:39:53 +0800

    换地方

--- 6.final.sh
+++ 6.final.sh
@@ -9,6 +9,11 @@ git config --global alias.ps push
 git config --global merge.ff true
 git config --global pull.ff true
 
+echo '====== Set default editor to vi ======'
 git config --global core.editor vi
+echo "
+export EDITOR=vi
+export VISUAL=$EDITOR
+" >> ~/.bashrc
 
 echo 'Please add '//npm.pkg.github.com/:_authToken=GITHUB_NPM_TOKEN' into ~/.npmrc, if you use github private npm registry'
