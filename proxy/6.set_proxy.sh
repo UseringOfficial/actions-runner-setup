8d8ccad79a46c22045cd9d327f9d57f625a12efa
delai <shidelai@gmail.com>
2025-01-20 16:14:02 +0800

    clash

--- proxy/6.set_proxy.sh
+++ proxy/6.set_proxy.sh
@@ -6,9 +6,17 @@ export https_proxy="http://127.0.0.1:7890"
 alias noproxy='unset all_proxy https_proxy http_proxy'
 " >> ~/.bashrc
 
+# download clash config
+# https://ikuuu.one/user/tutorial?os=linux&client=clash
+
+
+
 # auto start proxy
 sudo cp ./proxy/clash.service /etc/systemd/system/clash.service
 sudo systemctl daemon-reload
 sudo systemctl enable clash
 sudo systemctl start clash
 sudo systemctl status clash
+
+# enable https proxy for git
+# https://adangel.org/2020/10/15/github-behind-proxy/
