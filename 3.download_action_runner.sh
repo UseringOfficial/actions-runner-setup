09a445c16b845621c6917228aba7e461634ff3cb
delai <shidelai@gmail.com>
2025-02-15 23:11:35 +0800

    init runner

--- 3.download_action_runner.sh
+++ 3.download_action_runner.sh
@@ -1,4 +1,4 @@
-runner_is_download=`echo "9e883d210df8c6028aff475475a457d380353f9d01877d51cc01a17b2a91161d  ./runners/actions-runner-linux-x64.tar.gz" | shasum -a 256 -c`
+runner_is_download=`echo "b13b784808359f31bc79b08a191f5f83757852957dd8fe3dbfcc38202ccf5768  ./runners/aactions-runner-linux-x64.tar.gz" | shasum -a 256 -c`
 
 echo $runner_is_download
 
@@ -6,5 +6,5 @@ if [[ $runner_is_download == *"OK" ]]; then
     echo "runner is already download"
 else
     echo "runner is not download, start download"
-    curl -o runners/actions-runner-linux-x64.tar.gz -L "https://github.com/actions/runner/releases/download/v2.317.0/actions-runner-linux-x64-2.317.0.tar.gz"
+    curl -o ./runners/actions-runner-linux-x64.tar.gz -L https://github.com/actions/runner/releases/download/v2.322.0/actions-runner-linux-x64-2.322.0.tar.gz
 fi
